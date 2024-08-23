af7219d7a9c8ee93b726358712d123ed4781b229
NahueGon <71676618+NahueGon@users.noreply.github.com>
2024-08-23 10:44:44 -0300

    Update databse.sql

--- databse.sql
+++ databse.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS amb;
 USE amb;
 
-DROP TABLE IF EXISTS products;
-CREATE TABLE IF NOT EXISTS products(
+DROP TABLE IF EXISTS product;
+CREATE TABLE IF NOT EXISTS product(
     id              int(11) auto_increment not null,
     product         varchar(255),
     description     text,
     price           float,
 
-    CONSTRAINT pk_products PRIMARY KEY(id)
+    CONSTRAINT pk_product PRIMARY KEY(id)
 )ENGINE=InnoDb;
 
